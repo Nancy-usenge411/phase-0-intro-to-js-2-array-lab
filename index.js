f67bd781f6a603c9633d4e901bb67edc4f6fcb8c
Nancy Usenge <nancyusenge411@gmail.com>
2024-07-06 08:31:44 +0300

    commit js-2array lab

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Write your solution here!
+let cats = [ 'Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendCat( Ralph){
+    cats.push( 'Ralph')
+}
+
+function destructivelyPrependCat( Bob){
+    cats.unshift( 'Bob')
+}
+
+function destructivelyRemoveLastCat( Bob){
+    cats.pop( 'Bob')
+}
+
+function destructivelyRemoveFirstCat( Ralph){
+    cats.shift( 'Ralph')
+}
+
+function appendCat( Bob){
+    const copyOfCats =cats.slice( Bob);
+
+    copyOfCats.push( Bob);
+    return copyOfCats;
+}
+
+function prependCat( Arnold){
+    const newCats =[ 'Arnold', ...cats];
+    return newCats;
+}
+
+function removeLastCat( Garfield){
+    const newCats = cats.slice(0,-1)
+    return newCats;
+}
+
+function removeFirstCat( Arnold){
+    const newCats =cats.slice(1);
+    return newCats;
+}
